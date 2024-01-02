217d3eec7f295b2f347f68b98413eba3a759397e
David <david_gu@yahoo.com>
2024-01-01 21:26:16 -0800

    palindrome

--- dec28.js
+++ dec28.js
@@ -1,2 +1,47 @@
 // Dec 12 2023 Prac
-console.log('node test')
+
+// Two Sum (easy)
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+// Constraints:
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+const TwoSum = function(nums,target){
+    for(let i=0; i<nums.length; i++){
+        const val = target-nums[i]
+        if(nums.includes(val) && !(nums.indexOf(val)===i)){
+            return [i,nums.indexOf(val)]
+        }
+    }
+}
+
+const nums = [2,7,11,15]
+const target = 9
+// console.log(TwoSum(nums,target))
+
+//Palindrome Number
+//Given an integer x, return true if x is a palindrome , and false otherwise.
+const Palindrome = function(x){
+    let str = x.toString()
+    // console.log(str)
+    for(let i=str.length-1; i>0; i--){
+        // console.log(str.length)
+        console.log(str[i])
+    }
+}
+let x = 121
+console.log(Palindrome(x))
