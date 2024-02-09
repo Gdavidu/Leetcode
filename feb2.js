57b1bceb1129141c34d43cfae90924af423c5619
David <david_gu@yahoo.com>
2024-02-08 20:52:33 -0800

    figuring out mergetwoLists

--- feb2.js
+++ feb2.js
@@ -10,3 +10,18 @@ var mergeTwoLists = function(l1, l2) {
         return l2
     }
 };
+
+var mergeTwoLists = function(l1, l2) {
+    if (!l1) return l2;
+    else if (!l2) return l1;
+    else if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
+    }
+};
+
+// if there is only one list -> return that single list
+// else make the current node of the list = to the 'next node' of the first list, recursively passing 
