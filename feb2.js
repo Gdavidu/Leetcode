dad6704a6e9afe36e35827907cf4abc5e3a37d8b
David <david_gu@yahoo.com>
2024-02-10 21:05:43 -0800

    two numbers/ node list review

--- feb2.js
+++ feb2.js
@@ -24,4 +24,34 @@ var mergeTwoLists = function(l1, l2) {
 };
 
 // if there is only one list -> return that single list
-// else make the current node of the list = to the 'next node' of the first list, recursively passing 
+// else make the current node of the list = to the 'next node' of the first list, recursively passing
+
+
+// Two Numbers
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+var addTwoNumbers = function (l1, l2) {
+    let dummyHead = new ListNode(0); // Dummy head to simplify the code
+    let current = dummyHead;
+    let carry = 0;
+
+    while (l1 || l2) {
+        let x = l1 ? l1.val : 0;
+        let y = l2 ? l2.val : 0;
+        let sum = carry + x + y;
+
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+
+    return dummyHead.next;
+};
