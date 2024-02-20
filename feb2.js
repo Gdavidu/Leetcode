9f98740c51b1191c6cb69e732698a893183ae57a
David <david_gu@yahoo.com>
2024-02-19 16:35:48 -0800

    reaching points

--- feb2.js
+++ feb2.js
@@ -27,6 +27,32 @@ var mergeTwoLists = function(l1, l2) {
 // else make the current node of the list = to the 'next node' of the first list, recursively passing
 
 
+// Non-Recursive MergeTwoLists
+var mergeTwoLists = function(l1, l2) {
+    // if(!l1) return l2
+    // if(!l2) return l1
+    let curr = new ListNode()
+    const dummy = curr
+    while(l1 && l2){
+        if (l1.val<l2.val){
+            curr.next = l1
+            l1 = l1.next
+    }
+        else {
+            curr.next = l2
+            l2 = l2.next
+    }
+    curr = curr.next
+}
+if(l1){
+    curr.next = l1
+}
+if(l2){
+    curr.next = l2
+}
+return dummy.next
+};
+
 // Two Numbers
 // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
@@ -55,3 +81,31 @@ var addTwoNumbers = function (l1, l2) {
 
     return dummyHead.next;
 };
+
+//reaching points
+class Solution(object):
+    def reachingPoints(self, sx, sy, tx, ty):
+        """
+        :type sx: int
+        :type sy: int
+        :type tx: int
+        :type ty: int
+        :rtype: bool
+        """
+        # conditions when false
+        if sx > tx or sy > ty:
+            return False
+
+        # if we get to the final step
+        if sx == tx:
+            return (ty- sy) % sx == 0
+
+        if sy == ty:
+            return (tx - sx) % sy == 0
+
+        # recursive step
+        if tx > ty:
+            return self.reachingPoints(sx, sy, tx%ty, ty)
+
+        elif tx < ty:
+            return self.reachingPoints(sx, sy, tx, ty%tx)
