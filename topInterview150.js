be5afb1b2a1053582706bc3ec99b560add1054dd
David <david_gu2yahoo.com>
2024-03-02 17:26:43 -0800

    fixes to run error

--- topInterview150.js
+++ topInterview150.js
@@ -88,10 +88,10 @@ function removeDuplicates(nums) {
         return 0;
     }
 
-    let k = 1; // Initialize the count of unique elements to 1
+    let k = 1;
     for (let i = 1; i < nums.length; i++) {
         if (nums[i] !== nums[k - 1]) {
-            nums[k] = nums[i]; // Overwrite the next unique element
+            nums[k] = nums[i];
             k++;
         }
     }
