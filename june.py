3a1f55d4ccfd7b6fbde4082dcabc99d28c053aa7
gdavidu <david_gu@yahoo.com>
2024-07-19 23:52:42 -0700

    needle in haystack without indexOf method

--- june.py
+++ june.py
@@ -299,3 +299,15 @@ class Solution(object):
             s = s[1:]
         #     # print('count2: ', count2, 's: ', s)
         return count + count2
+# Find the Index of the First Occurrence in a String.
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
