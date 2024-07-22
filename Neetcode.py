8c725def2acc7200b788e48bce3d21277a295174
gdavidu <david_gu@yahoo.com>
2024-07-22 12:32:20 -0700

    solve

---
+++ Neetcode.py
@@ -0,0 +1,30 @@
+from typing import List
+
+# Duplicate Integer:
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+# My solution:
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)-1):
+            print(i)
+            if nums[i] == nums[i+1]:
+                return True
+        return False
+
+# Test cases:
+# nums = [1, 2, 3, 3]
+# print(Solution().hasDuplicate(nums))
+
+# Theirs:
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+#         hashset = set()
+
+#         for n in nums:
+#             if n in hashset:
+#                 return True
+#             hashset.add(n)
+#         return False
+# Notes:
