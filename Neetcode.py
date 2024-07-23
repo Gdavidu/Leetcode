01be8bdf9f65316567da6b5daf542c93e9c23ea7
gdavidu <david_gu@yahoo.com>
2024-07-22 23:52:01 -0700

    is anagram

--- Neetcode.py
+++ Neetcode.py
@@ -30,3 +30,30 @@ class Solution:
 # Notes:
 # Using a hashmap you sacrifice space complexity for an O(n) time complexity. My solution is a O(nlog(n)) but by using a hashsetto chekc values against, you achieve a O(n) time comp and a O(n) space comp as opposed to the prev O(1) space comp.
 # This solution entails checking whether the value is in the hashset and inputting it in the hashset if it isnt
+
+# Is Anagram
+# My Sol:
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sList = list(s)
+        tList = list(t)
+        sList.sort()
+        tList.sort()
+        if len(tList) != len(sList):
+            return False
+        for i in range(len(sList)):
+            if sList[i] != tList[i]:
+                return False
+        return True
+# Theirs:
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
