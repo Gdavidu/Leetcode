d2fb49351286ca0cbef28136b5cddf8ead11dc71
gdavidu <david_gu@yahoo.com>
2024-07-23 16:24:35 -0700

    review

--- Neetcode.py
+++ Neetcode.py
@@ -57,3 +57,6 @@ class Solution:
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
         return countS == countT
+#Notes:
+# used a hashmap again but new concept: the get method for sets that checks whether the calue exists or sets the default value as 0. Kind of remember this concept but in a different syntax, gonna review some of my old leetcode solutions. Back
+# to the solution: then using any of the two sets, check each value against the set of the other. I believe this means that hashsets are sorted? Nevermind its checking whether or not get returns a value cool.
