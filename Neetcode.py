c90ccde629d46058909c3d629061e6ffbcab3c8f
gdavidu <david_gu@yahoo.com>
2024-08-19 15:19:57 -0700

    ez medium

--- Neetcode.py
+++ Neetcode.py
@@ -289,3 +289,29 @@ class Solution:
                 return False
 
         return True
+# Product of Array Discluding Self
+# Mine
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(len(nums)):
+            product = 1
+            for j in range(len(nums)):
+                if j != i:
+                    product = product*nums[j]
+            res.append(product)
+        return res
+# Theirs:
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+# Notes
+# 
